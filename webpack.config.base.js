db1c72f3e29d4fda6b156996d59c4bd4c30a2995
carmine <hl13886803841@sina.com>
2020-02-22 17:04:25 +0800

    update

---
+++ webpack.config.base.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "CarminePink",
+      template: "src/assets/index.html" //引入我的模板
+    })
+  ]
+};
